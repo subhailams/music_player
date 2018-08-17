7f084a981e0e886eb1173d304e86292173acff6a
subha <subh16322.cs@rmkec.ac.in>
2018-08-17 18:51:39 +0530

    initial
    Initial commit

---
+++ app/src/main/java/com/example/sony/myprofile/ProfilePhoto.kt
@@ -0,0 +1,14 @@
+package com.example.sony.myprofile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilePhoto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_photo)
+        title = "My Profile Photo"
+
+    }
+}
